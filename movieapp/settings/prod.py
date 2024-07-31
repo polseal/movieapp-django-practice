ea3fe4013e89d9dd038a010a8090e21cc90cf0d0
alina.polezhaeva <alpo02@dfki.de>
2024-07-31 13:27:29 +0200

    prod database update

--- movieapp/settings/prod.py
+++ movieapp/settings/prod.py
@@ -5,4 +5,5 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+DATABASES = {'default': dj_database_url.config(default=database_url)}
+#DATABASES['default'] = dj_database_url.parse(database_url)
